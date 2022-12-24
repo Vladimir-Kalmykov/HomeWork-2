3ed36645d86a398b237e1181790354d29018a74b
Vladimir <bob.kalmykov@mail.ru>
2022-12-24 15:03:18 +0300

    Домашняя работа №2

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача №1.Напишите программу,которая принимает на вход трех значное число и на выходе показывает вторую цифру этого числа.
+
+// System.Console.WriteLine("Введите число: ");
+// string namber = Console.ReadLine();
+// if (namber.Length < 3)
+// {
+//     System.Console.WriteLine("Число должно содержыть не менее трех чисел");
+// }
+// else
+// {
+//     System.Console.WriteLine($"второе число: {namber[1]}");
+// }
+
+// Задача №2.Напишите программу, которая выводит третью цифру заданого числа или сообщает, что третьей цифры нет.
+
+// System.Console.WriteLine("Введите число: ");
+// string namber1 = Console.ReadLine();
+// if (namber1.Length < 3)
+// {
+//     System.Console.WriteLine("Третьего числа нет");
+// }
+// else
+// {
+//     System.Console.WriteLine($"третье число: {namber1[2]}");
+// }
+
+// Задача №3.Напишите программу,которая принимает на вход цифру,обозначающую день недели,и проверяет,являеться ли это выходным
+
+// System.Console.WriteLine("Введите число от 1 до 7");
+// int nomber2 = Convert.ToInt32(Console.ReadLine());
+// if(nomber2 == 1)
+// {
+//     System.Console.WriteLine("Понидельник, это рабочий день");
+// }
+// else if (nomber2 == 2)
+// {
+//     System.Console.WriteLine("Вторник, это рабочий день");
+// }
+// else if (nomber2 == 3)
+// {
+//     System.Console.WriteLine("Среда, это рабочий день");
+// }
+// else if (nomber2 == 4)
+// {
+//     System.Console.WriteLine("Четверг, это рабочий день");
+// }
+// else if (nomber2 == 5)
+// {
+//     System.Console.WriteLine("Пятница, это рабочий день");
+// }
+// else if (nomber2 == 6)
+// {
+//     System.Console.WriteLine("Суббота, это выходной день: Урааааа");
+// }
+// else if (nomber2 == 7)
+// {
+//     System.Console.WriteLine("Воскресенье, это выходной день: Урааааа");
+// }
+// else
+// {
+//     System.Console.WriteLine("Такого дня не существует");
+// }
